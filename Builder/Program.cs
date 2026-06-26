da9cb9bfabef327ce59d2f3cce72322df71aee24
Ravindrakale <ravind999@gmail.com>
2026-06-26 11:40:34 +0530

    Prototype and Builder Design patterns

---
+++ Builder/Program.cs
@@ -0,0 +1,123 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("-------------------------------List Of Toys--------------------------------------------");
+        var toyACreator = new ToyCreator(new ToyABuilder());
+        toyACreator.CreateToy();
+        DisplayToyDetails(toyACreator.GetToy());
+        var toyBCreator = new ToyCreator(new ToyBBuilder());
+        toyBCreator.CreateToy();
+        DisplayToyDetails(toyBCreator.GetToy());
+    }
+
+    public static void DisplayToyDetails(Toy toy)
+    {
+        Console.WriteLine($"Model: {toy.Model}");
+        Console.WriteLine($"Head: {toy.Head}");
+        Console.WriteLine($"Limb: {toy.Limbs}");
+        Console.WriteLine($"Body: {toy.Body}");
+        Console.WriteLine($"Legs: {toy.Legs}");
+        Console.WriteLine("------------------------------");
+    }
+}
+
+
+//This is the product class for Toy.
+public class Toy
+{
+    public string Model { get; set; }
+    public string Head { get; set; }
+    public string Limbs { get; set; }
+    public string Body { get; set; }
+    public string Legs { get; set; }
+}
+
+public interface IToyBuilder
+{
+    void SetModel();
+    void SetHead();
+    void SetLimbs();
+    void SetBody();
+    void SetLegs();
+    Toy GetToy();
+}
+
+public class ToyABuilder : IToyBuilder
+{
+    Toy toy = new Toy();
+    public void SetModel()
+    {
+        toy.Model = "TOY A";
+    }
+    public void SetHead()
+    {
+        toy.Head = "1";
+    }
+    public void SetLimbs()
+    {
+        toy.Limbs = "4";
+    }
+    public void SetBody()
+    {
+        toy.Body = "Plastic";
+    }
+    public void SetLegs()
+    {
+        toy.Legs = "2";
+    }
+    public Toy GetToy()
+    {
+        return toy;
+    }
+}
+
+public class ToyBBuilder : IToyBuilder
+{
+    Toy toy = new Toy();
+    public void SetModel()
+    {
+        toy.Model = "TOY B";
+    }
+    public void SetHead()
+    {
+        toy.Head = "1";
+    }
+    public void SetLimbs()
+    {
+        toy.Limbs = "4";
+    }
+    public void SetBody()
+    {
+        toy.Body = "Steel";
+    }
+    public void SetLegs()
+    {
+        toy.Legs = "4";
+    }
+    public Toy GetToy()
+    {
+        return toy;
+    }
+}
+
+public class ToyCreator
+{
+    private IToyBuilder _toyBuilder;
+    public ToyCreator(IToyBuilder toyBuilder)
+    {
+        _toyBuilder = toyBuilder;
+    }
+    public void CreateToy()
+    {
+        _toyBuilder.SetModel();
+        _toyBuilder.SetHead();
+        _toyBuilder.SetLimbs();
+        _toyBuilder.SetBody();
+        _toyBuilder.SetLegs();
+    }
+    public Toy GetToy()
+    {
+        return _toyBuilder.GetToy();
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,77 @@
+﻿public class Program
+{
+    /// <summary>
+    /// Prototype Design Pattern specifies the kind of objects to create using a prototypical instance and creates new objects by copying this prototype.
+    /// Deep Copy and Shallow copy
+    /// </summary>
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        p1.Age = 42;
+        p1.BirthDate = Convert.ToDateTime("1977-01-01");
+        p1.Name = "Jack Daniels";
+        p1.Id = new IdInfo(666);
+
+        // Perform a shallow copy of p1 and assign it to p2.
+        Person p2 = p1.ShallowCopy();
+        // Make a deep copy of p1 and assign it to p3.
+        Person p3 = p1.DeepCopy();
+
+        // Display values of p1, p2 and p3.
+        Console.WriteLine("Original values of p1, p2, p3:");
+        Console.WriteLine("   p1 instance values: ");
+        DisplayValues(p1);
+        Console.WriteLine("   p2 instance values:");
+        DisplayValues(p2);
+        Console.WriteLine("   p3 instance values:");
+        DisplayValues(p3);
+
+        // Change the value of p1 properties and display the values of p1,
+        // p2 and p3.
+        p1.Age = 32;
+        p1.BirthDate = Convert.ToDateTime("1900-01-01");
+        p1.Name = "Frank";
+        p1.Id.IdNumber = 7878;
+        Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+        Console.WriteLine("   p1 instance values: ");
+        DisplayValues(p1);
+        Console.WriteLine("   p2 instance values (reference values have changed):");
+        DisplayValues(p2);
+        Console.WriteLine("   p3 instance values (everything was kept the same):");
+        DisplayValues(p3);
+    }
+    public static void DisplayValues(Person p)
+    {
+        Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
+            p.Name, p.Age, p.BirthDate);
+        Console.WriteLine("      ID#: {0:d}", p.Id.IdNumber);
+    }
+}
+public class IdInfo
+{
+    public int IdNumber;
+
+    public IdInfo(int idNumber)
+    {
+        IdNumber = idNumber;
+    }
+}
+public class Person
+{
+    public IdInfo Id { get; set; }
+    public int Age;
+    public DateTime BirthDate;
+    public string Name;
+
+    public Person ShallowCopy()
+    {
+        return (Person)this.MemberwiseClone();
+    }
+
+    public Person DeepCopy()
+    {
+        Person clone = (Person)this.MemberwiseClone();
+        clone.Id = new IdInfo(Id.IdNumber);
+        return clone;
+    }
+}
